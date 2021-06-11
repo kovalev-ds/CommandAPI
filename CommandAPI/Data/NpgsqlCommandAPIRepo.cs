0fa54f3865ac8225e9042746e1382a6496b77c15
kovvdn <40830091+kovvdn@users.noreply.github.com>
2021-06-11 21:39:25 +0300

    Added DB Context

---
+++ CommandAPI/Data/NpgsqlCommandAPIRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Data
+{
+    public class NpgsqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+
+        public NpgsqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -5,20 +5,37 @@ using System.Threading.Tasks;
 using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(
+                opt => opt.UseNpgsql(builder.ConnectionString)
+            );
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, NpgsqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
